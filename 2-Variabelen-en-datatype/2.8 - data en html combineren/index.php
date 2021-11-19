f0645e6a85ec23cfafc15df4e076a7436904b4bd
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-19 20:51:27 +0100

    opdracht 2.8 af

---
+++ 2-Variabelen-en-datatype/2.8 - data en html combineren/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?php
+$voornaam1 = "Vincenzo";
+$voornaam2 = "Eric";
+$voornaam3 = "Recep aka GT";
+echo "<li>$voornaam1 </li> <li> $voornaam2</li>  <li>$voornaam3</li>";
+?>
+</body>
+</html>
+
+
+
