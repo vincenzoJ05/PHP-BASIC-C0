6f7304909d5e0736d7541bc78e1ccd1cafc048f3
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-20 17:17:42 +0100

    opdracht 3.2 af

---
+++ 3-Arrays/3.2 - Array waarde wijzigen/index.php
@@ -0,0 +1,13 @@
+<?php
+$myDogNames = [ "Pablo" , "Juan" , "Eric" ];
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br><br>" ;
+
+$myDogNames[0] = 'Demitridemarcusbartholomowjuniorthethird';
+echo $myDogNames[0] . "<br>";
+echo $myDogNames[1] . "<br>";
+echo $myDogNames[2] . "<br>";
+?>
+
+
