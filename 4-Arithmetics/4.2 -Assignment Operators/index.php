70a112cd1129fd9ffe9e86ad8f8e3762f46d2634
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-21 18:30:10 +0100

    opdr 4.2 af

---
+++ 4-Arithmetics/4.2 -Assignment Operators/index.php
@@ -0,0 +1,18 @@
+<?php
+$getal1 = 10;
+$getal1 += 5;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 -= 5;
+echo $getal1 . "<br>";
+
+$getal1 = 10;
+$getal1 *= 5;
+echo $getal1 . "<br>";
+
+
+$getal1 = 10;
+$getal1 /= 5;
+echo $getal1 . "<br>";
+?>
