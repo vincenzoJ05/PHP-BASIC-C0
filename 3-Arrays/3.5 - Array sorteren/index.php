1bfae54a8fc264dee39b33bda7d966cd05179f35
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-21 16:40:58 +0100

    opdracht 3.5 af

---
+++ 3-Arrays/3.5 - Array sorteren/index.php
@@ -0,0 +1,10 @@
+<?php
+    $coders =  [676,5345,78657,3,46,21,47,];
+    asort ($coders);
+    print_r($coders);
+
+    $laag_hoog = [3,21,46,47,676,5345,78657];
+    arsort ($laag_hoog);
+    print_r($laag_hoog);
+    ?>
+    
