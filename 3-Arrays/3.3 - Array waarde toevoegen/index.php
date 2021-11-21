40b9e08f8b87cfbd49c3f0d1c4280823e4562423
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-21 14:17:24 +0100

    opdracht 3.3 af

---
+++ 3-Arrays/3.3 - Array waarde toevoegen/index.php
@@ -0,0 +1,7 @@
+<?php
+$myHeroes = ['Antikomeetman' , 'Spoderman' , 'gigachin'];
+echo $myHeroes[0] . "<br>";
+echo $myHeroes[1] . "<br>";
+echo $myHeroes[2] . "<br>";
+array_push($myHeroes, 'GT');
+print_r($myHeroes);
