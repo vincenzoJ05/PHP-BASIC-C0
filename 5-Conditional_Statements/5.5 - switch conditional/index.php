2abc34c31e038268ba2b5d78638327cc2f474f70
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-22 22:18:36 +0100

    opdr 5.5 af

---
+++ 5-Conditional_Statements/5.5 - switch conditional/index.php
@@ -0,0 +1,23 @@
+<?php
+$score = 0;
+switch($score){
+    case 10;
+    echo "Nog even oefenen dan haal je vast alle punten!";
+    break;
+    case 8;
+    echo "Wow bijna alle punten nog  te gaan!";
+    break;
+    case 6;
+    echo "Ga zo door!";                                  
+    break;
+    case 4;
+    echo "Goed bezig, bijna alle punten!";
+    break;
+    case 2;
+    echo "Wow bijna alle punten nog 2 te gaan!";
+    break;
+    case 0;
+    echo "Gefeliciteert je hebt alles gehaald!";
+    break;
+}
+?>
