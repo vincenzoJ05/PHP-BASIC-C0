1b5e5ba526982ce89eb6b76da97b4af313ce503b
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-22 09:20:39 +0100

    opdr 5.1 af

---
+++ 5-Conditional_Statements/5.1 - Comparison Operators/index.php
@@ -0,0 +1,11 @@
+<?php
+$test1 = 12;
+$test2 = "gewoon tekst";
+var_dump($test1 == $test2);
+var_dump($test1 === $test2);
+var_dump($test1 != $test2);
+var_dump($test1 > $test2);
+var_dump($test1 < $test2);
+var_dump($test1 <> $test2);
+var_dump($test1 >= $test2);
+var_dump($test1 <= $test2);
