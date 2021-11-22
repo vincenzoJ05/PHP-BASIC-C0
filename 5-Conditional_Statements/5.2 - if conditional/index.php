47600b95fdd4620d20df7fbe2cf5f6c7356c42a1
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-22 09:46:26 +0100

    opdr 5.2 af

---
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -0,0 +1,18 @@
+<?php
+$test1 = 10;
+$test2 = 6;
+if($test1 == $test2); {
+    echo "Ongelijk" . "<br>";
+}
+
+if($test1 === $test2); {
+    echo "Identiek" . "<br>";
+}
+
+if($test1 > $test2); {
+    echo "test1 is groter dan test2" . "<br>";
+}
+
+if($test1 <> $test2); {
+    echo "Test 1 is groter of gelijk aan test2";
+}
