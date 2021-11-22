ec0ae04b7f636ce080b6526b4344d1d49ac5a059
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-22 15:27:57 +0100

    opdr 5.3 af

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,52 @@
+<?php
+$test1 = 3;
+$test2 = 6;
+
+if($test1 === $test2){
+    echo "gelijk";
+}
+else{
+    echo "False" . "<br>";
+}
+
+if($test1!=$test2){
+    echo "Het is ongelijk aan elkaar True" . "<br>";
+}
+else{
+    echo"werkt niet eh G";
+}
+
+if($test1===$test2){
+    echo "Het is identiek aan elkaar";
+}
+else{
+    echo"jeetje mineetje dit is ook niet identiek False" . "<br>";
+}
+
+if($test1>$test2){
+    echo "test1 niet groter dan test2";
+}
+else{
+    echo "Muchas gracis afisotros SSSSIIIIUUUUUUUU" . "<br>";
+}
+
+if($test1<$test2){
+    echo "Test2 is niet kleiner dan test1 True". "<br>";
+}
+else{
+    echo"deze is ook weer False wat jammer";
+}
+
+if($test1>=$test2){
+    echo "dit is voor de lol";
+}
+else{
+    echo"Het is niet groter of gelijk True" . "<br>";
+}
+
+if($test1<=$test2){
+    echo "test2 is niet kleiner of gelijk aan test1 True";
+}
+else{
+    echo"Dit is het antwoord lol";
+}
