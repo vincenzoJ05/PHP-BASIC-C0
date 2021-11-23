17209ee0b7db5be1ac1f9ba9a9adda168db40e1b
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-23 10:08:49 +0100

    opdr 6.2 af

---
+++ 6-Functions/6.2 - strlen()/index.php
@@ -0,0 +1,9 @@
+<?php
+
+function mijnNaamIsGroot(){
+    $voorNaam = "Vincenzo";
+    $achterNaam = "Jovanovic";
+    $geheleNaam = $voorNaam . " " . $achterNaam;
+    echo "Mijn naam is $geheleNaam";
+}
+mijnNaamIsGroot();
