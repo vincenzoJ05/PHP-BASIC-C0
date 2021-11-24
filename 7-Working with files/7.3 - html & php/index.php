bbaa8002143e1e6125e387f2ff91c2b7099e3fa2
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-24 14:27:32 +0100

    opdr 7.3 af

---
+++ 7-Working with files/7.3 - html & php/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div id="isMeerderJarig">
+        <?php
+        $leeftijd = 12;
+        if($leeftijd<18){
+            echo "Je hebt geen toegang tot deze website.";
+        }
+        ?>
+    </div>
+
+</body>
+</html>
