a4c68f0ca7bede2291c49d3614702a77d85df12d
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2021-11-24 13:40:10 +0100

    opdr 7.2 af

---
+++ 7-Working with files/7.2 - require/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<?php
+    require 'includes/database.php';
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+</head>
+<body>
+    <ul>
+        <li>menu-items 1</li>
+        <li>menu-items 2</li>
+        <li>menu-items 3</li>
+    </ul>
+</body>
+</html>
