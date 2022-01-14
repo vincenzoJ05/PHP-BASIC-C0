5b571a0cbc60dd0847c71cc555519bee780dac69
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-14 19:59:51 +0100

    print_r gebruikt

--- 3-Arrays/3.4 - Multidimensionaal/index.php
+++ 3-Arrays/3.4 - Multidimensionaal/index.php
@@ -1,5 +1,7 @@
 <?php
 $abcGetallen = [ ["a","b","c","d"], [24,56,78,90] ];
+print_r($abcGetallen);
+echo "<br>";
 echo $abcGetallen[0][0];
 echo $abcGetallen[1][1] . "<br>";
 echo $abcGetallen[0][2];
