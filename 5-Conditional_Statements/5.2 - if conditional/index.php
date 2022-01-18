d799dc8d6c626d3918a77006f8c6b7783e43819a
VINCENZO_JOVANOVIC <2151301@talnet.nl>
2022-01-18 17:04:36 +0100

    if statements geklaard

--- 5-Conditional_Statements/5.2 - if conditional/index.php
+++ 5-Conditional_Statements/5.2 - if conditional/index.php
@@ -1,18 +1,30 @@
 <?php
 $test1 = 10;
 $test2 = 6;
-if($test1 == $test2); {
-    echo "Ongelijk" . "<br>";
+if($test1 == $test2){
+    echo "<ul> <li>gelijk</li> </ul>";
 }
 
-if($test1 === $test2); {
-    echo "Identiek" . "<br>";
+if($test1 != $test2){
+    echo "<ul> <li>Ongelijk</li> </ul>";
 }
 
-if($test1 > $test2); {
-    echo "test1 is groter dan test2" . "<br>";
+if($test1 === $test2){
+    echo "<ul> <li>Identiek</li> </ul>";
 }
 
-if($test1 <> $test2); {
-    echo "Test 1 is groter of gelijk aan test2";
+if($test1 > $test2){
+    echo "<ul> <li>Test1 is groter dan test2</li> </ul>";
+}
+
+if($test1 < $test2){
+    echo "<ul> <li>Test1 is kleiner dan test2</li> </ul>";
+}
+
+if($test1 >= $test2){
+    echo "<ul> <li>Test 1 is groter of gelijk aan test2</li> </ul>";
+}
+
+if($test1 <= $test2){
+    echo "<ul> <li>Test1 is kleiner of gelijk aan test2</li> </ul>";
 }
